3ece53aab584c44cedbad897c18f03b8b6436dd9
Elena Sacramento <elena.sacramento80@gmail.com>
2025-01-08 19:40:21 +0000

    :bug: change server IP and port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 8081;
 
 const pool = new Pool({
     user: 'admin',
